1c47a99f2f6a8adae8c637863cc2aebd2790f5e0
Muhammed Shittu <devshittu@gmail.com>
2023-01-22 12:43:55 +0000

    Getting Started with Cheerio

--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -8,3 +8,9 @@ describe('parseRDF', () => {
     expect(parseRDF).to.be.a('function');
     });
 });
+
+it('should parse RDF content', () => {
+    const book = parseRDF(rdf);
+    expect(book).to.be.an('object');
+    expect(book).to.have.a.property('id', 132);
+    });
