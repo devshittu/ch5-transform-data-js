9f1a88a492df5270eef9a416c0ea1a7d2b66e8ce
Muhammed Shittu <devshittu@gmail.com>
2023-01-22 12:56:23 +0000

    Finalized and working code

--- databases/rdf-to-json.js
+++ databases/rdf-to-json.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const parseRDF = require('./lib/parse-rdf.js');
+const parseRDF = require('./lib/parse-rdf');
 const rdf = fs.readFileSync(process.argv[2]);
 const book = parseRDF(rdf);
+
 console.log(JSON.stringify(book, null, ' '));
